1c81d0d1c8327f4ed59791580dbd912396fbfae1
youssefmagdyy <youssefmagdy1910@gmail.com>
2022-05-03 10:23:12 +0200

    homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import { FiGithub } from 'react-icons/fi';
 
 const Homepage = () => {
   return (
     <div>
         <nav className='navbar'>
-            <a  href='https://github.com/youssefmagdyy/exercise-archive/'>
-                Git Repository
+            <a className='icon' href='https://github.com/youssefmagdyy/exercise-archive/'>
+                <FiGithub size={'2rem'}/>
             </a>
         </nav>
     <div className='home'>
