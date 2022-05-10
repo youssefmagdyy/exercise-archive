8c024764bfce87008d685e865782e156aac2e00a
youssefmagdyy <youssefmagdy1910@gmail.com>
2022-05-10 16:04:21 +0200

    removed backend

--- server/models/saved.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const savedSchema = mongoose.Schema({
-
-});
-
-const saved = mongoose.model('saved',savedSchema);
-export default saved;    
--- server/routes/exercises.js
+++
@@ -1,7 +0,0 @@
-import express  from "express";
-import { getExercises } from "../controllers/exercises.js";
-const router = express.Router();
-
-router.get('/',getExercises);
-
-export default router;
